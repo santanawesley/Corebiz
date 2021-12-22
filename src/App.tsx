d98fe4d27d9785c540985666fe5b0340ddd2a23d
Wesley Santana <wesley.santana@opah.com.br>
2021-12-22 19:19:31 -0300

    Creating Banner Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header';
+import { Banner } from './components/Banner';
 import { RegisterNews } from './components/RegisterNews';
 import { Footer } from './components/Footer';
 import styles from './App.module.css';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Banner />
       <RegisterNews />
       <Footer />
     </div>
---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BannerSlider from '../../assets/banner.svg';
+
+export const Banner = () => {
+  return (
+    <div>
+      <img src={BannerSlider} />
+    </div>
+  );
+}
