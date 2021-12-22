bae8ce7c030238f6cfa558fb2e20a11776bdc6c2
Wesley Santana <wesley.santana@opah.com.br>
2021-12-22 17:49:34 -0300

    Creating Email Log layout for news

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header';
+import { RegisterNews } from './components/RegisterNews';
 import { Footer } from './components/Footer';
 import styles from './App.module.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header />
+      <RegisterNews />
       <Footer />
     </div>
   )
